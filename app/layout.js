b7fc40a5d4f03c3d1c9f8e9c48321b8e2c69c932
Jack Hewertson <122547280+JackHewert@users.noreply.github.com>
2024-04-14 18:31:57 -0400

    third

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,27 @@
-import {NavBar} from '@/components/navbar/NavBar'
+import { NavBar } from '@/components/navbar/NavBar'
 import '@/css/globals.css'
 
 export const metadata = {
 	title: 'To Do List App',
 	description: 'Best to do list of the year in DMIT',
 }
+import mergeCss from '@/lib/utils/mergeCss';
 
-export default function RootLayout({children}) {
+const App = () => {
+	const className = mergeCss(['class1', 'class2', { 'class3': true }]);
+
 	return (
-		<html lang="en">
-			<link rel="icon" href="/svg/favicon.svg" type="image/svg+xml" />
-			<body className="bg-gray-200">
-				<NavBar />
-				{children}
-			</body>
-		</html>
-	)
-}
+		<div className={className}>
+			<html lang="en">
+				<link rel="icon" href="/svg/favicon.svg" type="image/svg+xml" />
+				<body className="bg-gray-200">
+					<NavBar />
+					{children}
+				</body>
+			</html>
+		</div>
+	);
+};
+
+export default App;
+
